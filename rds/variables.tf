4e133759d4f0704127ca3b64cfa5ea24e28fba85
Ranbeer Singh <ranbeersingh@Ranbeers-MacBook-Pro.local>
2019-08-13 09:53:02 +0100

    Variable rds_character_set_name added

--- rds/variables.tf
+++ rds/variables.tf
@@ -142,3 +142,8 @@ variable "character_set_name" {
   description = "(Optional) The character set name to use for DB encoding in Oracle instances. This can't be changed. See Oracle Character Sets Supported in Amazon RDS for more information."
   default     = ""
 }
+
+variable "rds_character_set_name" {
+  description = "(Optional) The character set name to use for DB encoding in Oracle instances. This can't be changed. See Oracle Character Sets Supported in Amazon RDS for more information."
+  default     = ""
+}
