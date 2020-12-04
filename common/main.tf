93de066516b8cce609f6e4073403373043114cda
Chris Kinsella <chris.kinsella@helkinsoft.com>
2020-12-04 15:36:35 +0000

    updated code via 0.12upgrade command

--- common/main.tf
+++ common/main.tf
@@ -1,12 +1,14 @@
-# terraform {
-#   # The configuration for this backend will be filled in by Terragrunt
-#   backend "s3" {}
-# }
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "s3" {
+  }
+}
 
-# provider "aws" {
-#   region  = var.region
-#   version = "~> 1.16"
-# }
+provider "aws" {
+  region  = var.region
+  version = "~> 1.16"
+}
 
 ####################################################
 # DATA SOURCE MODULES FROM OTHER TERRAFORM BACKENDS
@@ -14,12 +16,14 @@
 #-------------------------------------------------------------
 ### Getting current
 #-------------------------------------------------------------
-data "aws_region" "current" {}
+data "aws_region" "current" {
+}
 
 #-------------------------------------------------------------
 ### Getting the current running account id
 #-------------------------------------------------------------
-data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {
+}
 
 #-------------------------------------------------------------
 ### Getting the vpc details
@@ -27,7 +31,7 @@ data "aws_caller_identity" "current" {}
 data "terraform_remote_state" "vpc" {
   backend = "s3"
 
-  config {
+  config = {
     bucket = var.remote_state_bucket_name
     key    = "vpc/terraform.tfstate"
     region = var.region
@@ -40,7 +44,7 @@ data "terraform_remote_state" "vpc" {
 data "terraform_remote_state" "nat" {
   backend = "s3"
 
-  config {
+  config = {
     bucket = var.remote_state_bucket_name
     key    = "natgateway/terraform.tfstate"
     region = var.region
@@ -53,7 +57,7 @@ data "terraform_remote_state" "nat" {
 data "terraform_remote_state" "monitor" {
   backend = "s3"
 
-  config {
+  config = {
     bucket = var.remote_state_bucket_name
     key    = "shared-monitoring/terraform.tfstate"
     region = var.region
@@ -66,7 +70,7 @@ data "terraform_remote_state" "monitor" {
 data "terraform_remote_state" "security-groups" {
   backend = "s3"
 
-  config {
+  config = {
     bucket = var.remote_state_bucket_name
     key    = "security-groups/terraform.tfstate"
     region = var.region
@@ -106,13 +110,13 @@ data "aws_ami" "amazon_ami" {
 
 locals {
   account_id                   = data.aws_caller_identity.current.account_id
-  vpc_id                       = data.terraform_remote_state.vpc.vpc_id
-  cidr_block                   = data.terraform_remote_state.vpc.vpc_cidr_block
-  allowed_cidr_block           = [data.terraform_remote_state.vpc.vpc_cidr_block]
-  internal_domain              = data.terraform_remote_state.vpc.private_zone_name
-  private_zone_id              = data.terraform_remote_state.vpc.private_zone_id
-  external_domain              = data.terraform_remote_state.vpc.public_zone_name
-  public_zone_id               = data.terraform_remote_state.vpc.public_zone_id
+  vpc_id                       = data.terraform_remote_state.vpc.outputs.vpc_id
+  cidr_block                   = data.terraform_remote_state.vpc.outputs.vpc_cidr_block
+  allowed_cidr_block           = [data.terraform_remote_state.vpc.outputs.vpc_cidr_block]
+  internal_domain              = data.terraform_remote_state.vpc.outputs.private_zone_name
+  private_zone_id              = data.terraform_remote_state.vpc.outputs.private_zone_id
+  external_domain              = data.terraform_remote_state.vpc.outputs.public_zone_name
+  public_zone_id               = data.terraform_remote_state.vpc.outputs.public_zone_id
   common_name                  = "${var.environment_identifier}-${var.iaps_app_name}"
   lb_account_id                = var.lb_account_id
   region                       = var.region
@@ -125,68 +129,73 @@ locals {
   s3_lb_policy_file            = "../policies/s3_alb_policy.json"
   environment                  = var.environment_type
   admin_user                   = "${var.iaps_app_name}${var.environment_type}"
-  tags                         = merge(data.terraform_remote_state.vpc.tags, map("sub-project", "${var.iaps_app_name}"))
-  ssh_deployer_key             = data.terraform_remote_state.vpc.ssh_deployer_key
+  tags = merge(
+    data.terraform_remote_state.vpc.outputs.tags,
+    {
+      "sub-project" = var.iaps_app_name
+    },
+  )
+  ssh_deployer_key = data.terraform_remote_state.vpc.outputs.ssh_deployer_key
 
   app_hostnames = {
     internal = "${var.iaps_app_name}-int"
-    external = "${var.iaps_app_name}"
+    external = var.iaps_app_name
   }
 
   sg_map_ids = {
-    bastion_in_sg_id       = data.terraform_remote_state.security-groups.sg_ssh_bastion_in_id
-    sg_iaps_api_in         = data.terraform_remote_state.security-groups.sg_iaps_api_in
-    sg_iaps_api_out        = data.terraform_remote_state.security-groups.sg_iaps_api_out
-    sg_iaps_db_in          = data.terraform_remote_state.security-groups.sg_iaps_db_in
-    sg_iaps_external_lb_in = data.terraform_remote_state.security-groups.sg_iaps_external_lb_in
+    bastion_in_sg_id       = data.terraform_remote_state.security-groups.outputs.sg_ssh_bastion_in_id
+    sg_iaps_api_in         = data.terraform_remote_state.security-groups.outputs.sg_iaps_api_in
+    sg_iaps_api_out        = data.terraform_remote_state.security-groups.outputs.sg_iaps_api_out
+    sg_iaps_db_in          = data.terraform_remote_state.security-groups.outputs.sg_iaps_db_in
+    sg_iaps_external_lb_in = data.terraform_remote_state.security-groups.outputs.sg_iaps_external_lb_in
   }
 
   private_subnet_map = {
-    az1 = data.terraform_remote_state.vpc.vpc_private-subnet-az1,
-    az2 = data.terraform_remote_state.vpc.vpc_private-subnet-az2,
-    az3 = data.terraform_remote_state.vpc.vpc_private-subnet-az3,
+    az1 = data.terraform_remote_state.vpc.outputs.vpc_private-subnet-az1
+    az2 = data.terraform_remote_state.vpc.outputs.vpc_private-subnet-az2
+    az3 = data.terraform_remote_state.vpc.outputs.vpc_private-subnet-az3
   }
 
   public_subnet_map = {
-    az1 = data.terraform_remote_state.vpc.vpc_public-subnet-az1,
-    az2 = data.terraform_remote_state.vpc.vpc_public-subnet-az2,
-    az3 = data.terraform_remote_state.vpc.vpc_public-subnet-az3,
+    az1 = data.terraform_remote_state.vpc.outputs.vpc_public-subnet-az1
+    az2 = data.terraform_remote_state.vpc.outputs.vpc_public-subnet-az2
+    az3 = data.terraform_remote_state.vpc.outputs.vpc_public-subnet-az3
   }
 
   public_cidr_block = [
-    data.terraform_remote_state.vpc.vpc_public-subnet-az1-cidr_block,
-    data.terraform_remote_state.vpc.vpc_public-subnet-az2-cidr_block,
-    data.terraform_remote_state.vpc.vpc_public-subnet-az3-cidr_block,
+    data.terraform_remote_state.vpc.outputs.vpc_public-subnet-az1-cidr_block,
+    data.terraform_remote_state.vpc.outputs.vpc_public-subnet-az2-cidr_block,
+    data.terraform_remote_state.vpc.outputs.vpc_public-subnet-az3-cidr_block,
   ]
 
   private_cidr_block = [
-    data.terraform_remote_state.vpc.vpc_private-subnet-az1-cidr_block,
-    data.terraform_remote_state.vpc.vpc_private-subnet-az2-cidr_block,
-    data.terraform_remote_state.vpc.vpc_private-subnet-az3-cidr_block,
+    data.terraform_remote_state.vpc.outputs.vpc_private-subnet-az1-cidr_block,
+    data.terraform_remote_state.vpc.outputs.vpc_private-subnet-az2-cidr_block,
+    data.terraform_remote_state.vpc.outputs.vpc_private-subnet-az3-cidr_block,
   ]
 
   db_cidr_block = [
-    data.terraform_remote_state.vpc.vpc_db-subnet-az1-cidr_block,
-    data.terraform_remote_state.vpc.vpc_db-subnet-az2-cidr_block,
-    data.terraform_remote_state.vpc.vpc_db-subnet-az3-cidr_block,
+    data.terraform_remote_state.vpc.outputs.vpc_db-subnet-az1-cidr_block,
+    data.terraform_remote_state.vpc.outputs.vpc_db-subnet-az2-cidr_block,
+    data.terraform_remote_state.vpc.outputs.vpc_db-subnet-az3-cidr_block,
   ]
 
   db_subnet_ids = [
-    data.terraform_remote_state.vpc.vpc_db-subnet-az1,
-    data.terraform_remote_state.vpc.vpc_db-subnet-az2,
-    data.terraform_remote_state.vpc.vpc_db-subnet-az3,
+    data.terraform_remote_state.vpc.outputs.vpc_db-subnet-az1,
+    data.terraform_remote_state.vpc.outputs.vpc_db-subnet-az2,
+    data.terraform_remote_state.vpc.outputs.vpc_db-subnet-az3,
   ]
 
   nat_gateways_ips = [
-    "${data.terraform_remote_state.nat.natgateway_common-nat-public-ip-az1}/32",
-    "${data.terraform_remote_state.nat.natgateway_common-nat-public-ip-az2}/32",
-    "${data.terraform_remote_state.nat.natgateway_common-nat-public-ip-az3}/32",
+    "${data.terraform_remote_state.nat.outputs.natgateway_common-nat-public-ip-az1}/32",
+    "${data.terraform_remote_state.nat.outputs.natgateway_common-nat-public-ip-az2}/32",
+    "${data.terraform_remote_state.nat.outputs.natgateway_common-nat-public-ip-az3}/32",
   ]
 
   bastion_cidrs = [
-    data.terraform_remote_state.vpc.bastion_vpc_public_cidr["az1"],
-    data.terraform_remote_state.vpc.bastion_vpc_public_cidr["az2"],
-    data.terraform_remote_state.vpc.bastion_vpc_public_cidr["az3"],
+    data.terraform_remote_state.vpc.outputs.bastion_vpc_public_cidr["az1"],
+    data.terraform_remote_state.vpc.outputs.bastion_vpc_public_cidr["az2"],
+    data.terraform_remote_state.vpc.outputs.bastion_vpc_public_cidr["az3"],
   ]
 }
 
@@ -197,7 +206,12 @@ resource "aws_security_group" "vpc-sg-outbound" {
   name        = "${local.common_name}-sg-outbound"
   description = "security group for ${local.common_name}-traffic"
   vpc_id      = local.vpc_id
-  tags        = merge(local.tags, map("Name", "${local.common_name}-outbound-traffic"))
+  tags = merge(
+    local.tags,
+    {
+      "Name" = "${local.common_name}-outbound-traffic"
+    },
+  )
 
   lifecycle {
     create_before_destroy = true
@@ -228,7 +242,8 @@ resource "aws_security_group_rule" "https" {
 # ### S3 bucket for config
 # #--------------------------------------------
 module "s3config_bucket" {
-  source         = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git//modules//s3bucket//s3bucket_without_policy?ref=terraform-0.12"
+  source         = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=pre-shared-vpc//modules//s3bucket//s3bucket_without_policy"
   s3_bucket_name = local.common_name
   tags           = local.tags
 }
+
