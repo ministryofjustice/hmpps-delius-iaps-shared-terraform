04b5c97c3bb71f79654da3ae6d73589d9cd022d2
Chris Kinsella <chris.kinsella@helkinsoft.com>
2021-01-26 17:05:28 +0000

    updated cloudwatch logs to 1 log group per log to allow more detailed filtering

--- ec2/cloudwatch.tf
+++ ec2/cloudwatch.tf
@@ -9,3 +9,91 @@ resource "aws_cloudwatch_log_group" "iaps_log_group" {
   )
 }
 
+resource "aws_cloudwatch_log_group" "iaps_log_group_cloudwatch_agent" {
+  name              = "${var.environment_name}/IAPS/amazon-cloudwatch-agent.log" 
+  retention_in_days = var.log_retention
+  tags = merge(
+    local.tags,
+    {
+      "Name" = "${var.environment_name}/IAPS/amazon-cloudwatch-agent.log"
+    },
+  )
+}
+
+resource "aws_cloudwatch_log_group" "iaps_log_group_nginx_access" {
+  name              = "${var.environment_name}/IAPS/access.log" 
+  retention_in_days = var.log_retention
+  tags = merge(
+    local.tags,
+    {
+      "Name" = "${var.environment_name}/IAPS/access.log" 
+    },
+  )
+}
+
+resource "aws_cloudwatch_log_group" "iaps_log_group_nginx_error" {
+  name              = "${var.environment_name}/IAPS/error.log"
+  retention_in_days = var.log_retention
+  tags = merge(
+    local.tags,
+    {
+      "Name" = "${var.environment_name}/IAPS/error.log" 
+    },
+  )
+}
+
+resource "aws_cloudwatch_log_group" "iaps_log_group_xmltransfer" {
+  name              = "${var.environment_name}/IAPS/i2n-xmltransfer.log" 
+  retention_in_days = var.log_retention
+  tags = merge(
+    local.tags,
+    {
+      "Name" = "${var.environment_name}/IAPS/i2n-xmltransfer.log"
+    },
+  )
+}
+
+resource "aws_cloudwatch_log_group" "iaps_log_group_imiapsif" {
+  name              = "${var.environment_name}/IAPS/imiapsif.log" 
+  retention_in_days = var.log_retention
+  tags = merge(
+    local.tags,
+    {
+      "Name" = "${var.environment_name}/IAPS/imiapsif.log"
+    },
+  )
+}
+
+resource "aws_cloudwatch_log_group" "iaps_log_group_backup" {
+  name              = "${var.environment_name}/IAPS/backup.log" 
+  retention_in_days = var.log_retention
+  tags = merge(
+    local.tags,
+    {
+      "Name" = "${var.environment_name}/IAPS/backup.log"
+    },
+  )
+}
+
+resource "aws_cloudwatch_log_group" "iaps_log_group_system_events" {
+  name              = "${var.environment_name}/IAPS/system-events" 
+  retention_in_days = var.log_retention
+  tags = merge(
+    local.tags,
+    {
+      "Name" = "${var.environment_name}/IAPS/system-events"
+    },
+  )
+}
+
+
+resource "aws_cloudwatch_log_group" "iaps_log_group_application_events" {
+  name              = "${var.environment_name}/IAPS/application-events" 
+  retention_in_days = var.log_retention
+  tags = merge(
+    local.tags,
+    {
+      "Name" = "${var.environment_name}/IAPS/application-events"
+    },
+  )
+}
