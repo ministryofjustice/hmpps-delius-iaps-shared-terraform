553e70eb8a2418b93b3e5a81ee6a9fa222152c5c
Chris Kinsella <chris.kinsella@helkinsoft.com>
2021-01-27 14:33:18 +0000

    Added IAPS Logs metrics and alarms

--- monitoring/locals.tf
+++ monitoring/locals.tf
@@ -3,5 +3,15 @@ locals {
   lambda_name_batch       = "${var.environment_name}-notify-iaps-slack-channel-batch"
   quiet_period_start_hour = "0"
   quiet_period_end_hour   = "3"
+
+  # metrics  
+  iaps_nginx_error_log_sum_metric_name = "NginxErrorLogEventSum"
+  
+  iaps_xmltransfer_log_sum_metric_name = "XMLTransferErrorLogEventSum"
+  iaps_imiapsif_log_sum_metric_name    = "IMIAPSIFErrorLogEventSum"
+
+  iaps_cloudwatch_agent_log_sum_metric_name = "CloudwatchAgentErrorLogEventSum"
+  iaps_config_backup_log_sum_metric_name    = "ConfigBackupLogEventSum"
+
 }
 
